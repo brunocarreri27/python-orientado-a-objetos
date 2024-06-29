050e7f6668a5e03ce2e53d7e57568c86f33609ba
brunocarreri27 <beatsound015@gmail.com>
2024-06-29 17:55:10 -0300

    Primeiro código Python Orientado a objetos

---
+++ main.py
@@ -0,0 +1,18 @@
+from pessoas import Pessoa
+
+sexo = input("Qual o seu sexo? : ").lower()
+peso = float(input("Qual o seu peso em KG? : "))
+
+pessoa = Pessoa(sexo=sexo, peso=peso)
+
+
+resultado1 = pessoa.calcularHomem()
+resultado2 = pessoa.calcularMulher()
+
+if resultado1:
+        print(f"O gasto diário calórico é {resultado1:.2f} calorias.")
+
+
+elif resultado2:
+    print(f"O gasto diário calórico é {resultado2:.2f} calorias.")
+
---
+++ pessoas.py
@@ -0,0 +1,14 @@
+class Pessoa:
+    def __init__(self, sexo, peso):
+        self.sexo = sexo
+        self.peso = peso
+
+    def calcularHomem(self):
+          if self.sexo == "masculino":
+                gasto_diario = (0.063 * self.peso + 2.896) * 239
+                return gasto_diario
+    
+    def calcularMulher(self):
+            if self.sexo == "feminino":
+                gasto_diario = (0.062 * self.peso + 2.036) * 239
+                return gasto_diario
